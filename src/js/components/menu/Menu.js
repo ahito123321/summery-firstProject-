efcda69fc7aae65caea20709d97ec5e5f6911642
ahito123321 <ahito.123321@gmail.com>
2018-09-07 18:53:42 +0300

    ..

---
+++ src/js/components/menu/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Menu extends React.Component{
+    render(){
+        return(
+            <div className="menu-app-wrap">
+                <div className="menu-logo-wrap">
+                    <div className="menu-logo"></div>
+                    <h1 className="menu-text-a">SUMMARY</h1>
+                </div>
+                <div className="menu_items">
+                    <a className="menu_item menu_item-active" href="#section-1-wrap-slider">Home</a>
+                    <a className="menu_item" href="#section-2-wrap-about-me">About</a>
+                    <a className="menu_item" href="#section-3-skills">Skills</a>
+                    <a className="menu_item" href="#section-4-education">Education</a>
+                    <a className="menu_item" href="#section-5-portfolio">Portfolio</a>
+                    <a className="menu_item" href="#section-6-wrap-interests">Interests</a>
+                    <a className="menu_item" href="#section-7-contact-us-container">Contacts</a>
+                </div>          
+                <div className="menu_footer">
+                    <h3>.....................</h3> 
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Menu;
